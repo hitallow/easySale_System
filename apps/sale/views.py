626b2ea77d8a8bacb7f81857bc1e2c868481325c
hitallow <hitallo91@gmail.com>
2018-10-26 23:11:09 -0300

    new pages

--- apps/sale/views.py
+++ apps/sale/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request , 'home.html')
+def login(request):
+    return render(request , 'login.html')
