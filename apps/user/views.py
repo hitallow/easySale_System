eb2c265980d6c0d81c1baaa5ad6b5419428f922e
hitallow <hitallow@gmail.com>
2018-11-22 11:10:26 -0300

    integrando o backend de detalhes dos produtos #4

--- apps/user/views.py
+++ apps/user/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Views da app User.
 root ='user/'
+
+
 def login(request):
-    return render(request , root+'login.html')
+    return render(request, root+'login.html')
