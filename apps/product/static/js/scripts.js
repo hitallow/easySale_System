ef9c6a60e696267d781f63f0ddab8e6cb9e116e9
hitallow <hitallo91@gmail.com>
2018-12-03 10:41:06 -0300

    adcionando carrousel #10

--- apps/product/static/js/scripts.js
+++ apps/product/static/js/scripts.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('#id_cpf').mask('000.000.000-00', {reverse: true});
+
+
+});
+
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 280) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
+});
+
+$(function(){
+    var SetCarouselHeight = function() {
+        $("#carouselExampleIndicators .item > img").height(function(){
+            return $("#myCarousel").width() * 0.5;
+        });
+    }
+
+    SetCarouselHeight();
+    $(window).resize(function(){
+        SetCarouselHeight();
+    }); 
 });
