aa3b24a454b6357ee720e060375a8dc6daee64da
hitallow <hitallo91@gmail.com>
2018-12-05 10:12:45 -0300

    user and product implementation #10 #8 #6

--- apps/product/forms.py
+++ apps/product/forms.py
@@ -1,11 +1,33 @@
 from django import forms
 from .models import Product
+from django.forms import (
+    NumberInput,TextInput, CheckboxInput,CheckboxSelectMultiple,Select,FileInput
+    )
 
 
+FAVORITE_COLORS_CHOICES = (
+	('outros','Outros'),
+    ('informatica', 'Informática'),
+    ('som', 'Som'),
+    ('roupas', 'Roupas'),
+    ('computadores','Computadores'),
+    ('comidas','comidas')
+
+)
 class InsertProduct(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
         exclude = ['cpfUserPost', 'slug']
+        widgets = {
+            'name': TextInput( attrs={'placeholder': 'nome do produto','class':'form-control'}),
+            'description':TextInput(attrs={'placeholder':'Descrição do produto','class':'form-control'}),
+            'preco':NumberInput(attrs={'placeholder':'Preco do produto','class':'form-control','min':'0.1'}),
+            'juros':CheckboxInput(),
+            'maxParc':NumberInput( attrs={'min':'1','max':'24' ,'class':'form-control'}),
+            'tipyDept': Select(choices=FAVORITE_COLORS_CHOICES, attrs={'class':'custom-select'}),
+            'image':FileInput(attrs={'class':'custom-file-input'})
+
+        }
 
 
--- apps/product/views.py
+++ apps/product/views.py
@@ -9,27 +9,25 @@ from .models import Product
 def index(request):
     return render(request, 'home.html')
 
-@login_required()
+
 def listAllProducts(request):
     produtos = Product.objects.all()
-
     context ={
         'produtos': produtos
     }
 
     return render(request ,'listProducts.html', context)
+@login_required()
 def cadastrar(request):
     if request.method == 'POST':
         form = InsertProduct(request.POST)
         if (form.is_valid()):
-            print('Form flag')
             produto = form.save(commit=False)
             produto.slug = slugify(form.cleaned_data['name'])
             produto.cpfUserPost = request.user
             produto.save()
             return redirect('product:allProducts')
     else:
-        print("form limpo")
         form = InsertProduct()
     context = {
         'form': form
